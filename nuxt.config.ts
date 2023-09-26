edee53ebc1e90b9a327aece615d288c98bd23f79
Daniel Meiborg <mail@danielmeiborg.com>
2023-09-26 11:52:37 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+  },
+  devtools: true,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@tailwindcss/typography",
+    "nuxt-vuefire",
+    "@vueuse/nuxt",
+  ],
+  app: {
+    head: {
+      title: "Schachclub Weiße Dame Ulm",
+      htmlAttrs: {
+        lang: "de",
+      },
+    },
+  },
+  ssr: false,
+  vuefire: {
+    auth: true,
+    config: {
+      apiKey: "AIzaSyBLsJJIbM9dujWpKC-TfXzgIMq-XGCnhCM",
+      authDomain: "seating-planner-7a7b1.firebaseapp.com",
+      projectId: "seating-planner-7a7b1",
+      storageBucket: "seating-planner-7a7b1.appspot.com",
+      messagingSenderId: "728143838714",
+      appId: "1:728143838714:web:8e06d40d7a033108caf843"
+    }
+  },
+  vite: {
+    plugins: [
+      ReactivityTransform()
+    ]
+  }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    daisyui: {
+    },
+    plugins: [require("@tailwindcss/typography"),
+    require("daisyui")],
+};
