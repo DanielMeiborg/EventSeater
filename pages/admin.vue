27acc8c506eb26c72fe0334e6a2ff4806f1beecd
Daniel Meiborg <mail@danielmeiborg.com>
2023-10-05 11:27:59 +0200

    feat: admins can create organizations

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>Admin</div>
+    <button class="btn btn-outline btn-wide" v-if="organization === ''" @click="create_organization()">Organisation
+        erstellen</button>
+    <div v-else class="flex flex-col items-center w-full">
+        <div class="flex">
+            <span class="badge badge-neutral mr-5">Organisation</span>
+            <span class="badge badge-outline">{{ organization }}</span>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-
 const auth = useFirebaseAuth();
+const user = $(useCurrentUser());
 if (auth) {
-    if (!useCurrentUser().value) {
+    if (user === null) {
         console.log("Currently not logged in");
         const { isSignInWithEmailLink, signInWithEmailLink } = await import("firebase/auth");
         if (isSignInWithEmailLink(auth, window.location.href)) {
@@ -34,6 +41,48 @@ if (auth) {
         }
     }
 }
+let organization = $(useLocalStorage("organization", ""));
+if (organization === "") {
+    const { getFirestore, doc, getDoc } = await import("firebase/firestore/lite");
+    const db = getFirestore(useFirebaseApp());
+    if (user !== null && user !== undefined) {
+        const docRef = doc(db, "admins/" + user.email);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            organization = docSnap.data().organization;
+        }
+    }
+}
+
+const create_organization = async () => {
+    const temporaryOrganization = window.prompt("Bitte gib den Namen deiner neuen Organisation ein") || "";
+    if (user === null || user === undefined) {
+        useBanner("Du musst angemeldet sein, um eine Organisation zu erstellen", "error");
+        return;
+    }
+    if (temporaryOrganization !== "") {
+        const { getFirestore, doc, setDoc, updateDoc } = await import("firebase/firestore/lite");
+        const db = getFirestore(useFirebaseApp());
+        const docRef = doc(db, "organizations/" + temporaryOrganization);
+        await setDoc(docRef, {
+            admin: user.email,
+            admin_uid: user.uid,
+        }).then(async () => {
+            await updateDoc(doc(db, "admins/" + user.email), {
+                organization: temporaryOrganization,
+            }).then(() => {
+                organization = temporaryOrganization;
+                useBanner("Organisation erstellt", "success");
+            }).catch((error) => {
+                useBanner("Organisation konnte nicht erstellt werden", "error");
+                console.log(error);
+            })
+        }).catch((error) => {
+            useBanner("Organisation konnte nicht erstellt werden", "error");
+            console.log(error);
+        });
+    }
+};
 
 definePageMeta({
     title: "Admin-Übersicht",
