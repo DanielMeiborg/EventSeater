dc93c7b43b4d377a0ef9b1fc3e3070d7a48873dd
Daniel Meiborg <mail@danielmeiborg.com>
2023-11-27 20:10:20 +0100

    fix: remove lorem ipsum

--- pages/create-plan.vue
+++ pages/create-plan.vue
@@ -40,22 +40,8 @@
                             placeholder='{"users": ["a", "b", "c"], "preferences": {"a": ["b"]}, "tables": [2, 2]}'
                             class="textarea textarea-bordered textarea-accent w-full" />
                     </div>
-                    <h3 class="text-2xl font-bold pt-8 pb-3">Details zum Algorithmus</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>
-            <p class="max-w-prose mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt
-                ut labore et
-                dolore
-                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo
-                consequat.
-            </p>
         </div>
         <div>
             <Plan v-if="results !== null && bestScore !== null" :results="results" :bestScore="bestScore" />
