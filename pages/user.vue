14ae87b09f0bebe6f77b8e316ae6e38282a57084
Daniel Meiborg <mail@danielmeiborg.com>
2023-12-17 11:38:28 +0100

    feat: add info message about autosaving preferences

--- pages/user.vue
+++ pages/user.vue
@@ -14,7 +14,14 @@
 
                 <!-- <h2 class="text-3xl font-bold pt-8 pb-3">Tischwünsche abgeben</h2> -->
                 <!-- <button class="btn btn-primary btn-wide" @click="updatePreferences()">Liste aktualisieren</button> -->
-
+                <div role="alert" class="alert alert-info">
+                    <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        class="stroke-current shrink-0 w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg> -->
+                    <span>Tischwünsche werden automatisch gespeichert</span>
+                </div>
                 <div class="overflow-x-auto pt-3">
                     <table class="table">
                         <tbody>
